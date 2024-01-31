e007395e071a787095709f823de6dccb745724b9
Lê Ý <ylvn@mbp-cua-le-2.0.1.168.192.in-addr.arpa>
2024-02-01 01:33:26 +0700

    Register

--- src/pages/coaches/CoachRegister.vue
+++ src/pages/coaches/CoachRegister.vue
@@ -4,30 +4,68 @@
     <form class="flex flex-col gap-2" @submit.prevent="onSubmit">
       <label class="grid grid-cols-[minmax(120px,auto)_1fr] items-baseline">
         <strong>First name:</strong>
-        <input type="text" v-model="coach.firstName" />
+        <input
+          type="text"
+          v-model="validateObj.firstName.val"
+          :class="{
+            [invalidClass]: !validateObj.firstName.isValid
+          }"
+          @change="removeValidate('firstName')"
+        />
       </label>
       <label class="grid grid-cols-[minmax(120px,auto)_1fr] items-baseline">
         <strong>Last name:</strong>
-        <input type="text" v-model.trim="coach.lastName" />
+        <input
+          type="text"
+          v-model.trim="validateObj.lastName.val"
+          @change="removeValidate('lastName')"
+          :class="{
+            [invalidClass]: !validateObj.lastName.isValid
+          }"
+        />
       </label>
       <div class="grid grid-cols-[minmax(120px,auto)_1fr] items-baseline">
         <strong>Areas:</strong>
         <div class="flex gap-3">
           <label v-for="area in areas" :key="area" class="flex gap-1 items-center select-none">
-            <input type="checkbox" v-model="coach.areas" :value="area" />
+            <input
+              type="checkbox"
+              v-model="validateObj.areas.val"
+              :value="area"
+              @change="removeValidate('areas')"
+            />
             <span class="capitalize">{{ area }}</span>
           </label>
         </div>
+        <small class="text-red-500" v-if="!validateObj.areas.isValid"
+          >Please select at least 1 area</small
+        >
       </div>
       <div class="grid grid-cols-[minmax(120px,auto)_1fr] items-end">
         <label for="description">
           <strong>Description: </strong>
         </label>
-        <textarea id="description" type="text" v-model.trim="coach.description" rows="3"></textarea>
+        <textarea
+          id="description"
+          type="text"
+          v-model.trim="validateObj.description.val"
+          rows="3"
+          :class="{
+            [invalidClass]: !validateObj.description.isValid
+          }"
+          @change="removeValidate('description')"
+        ></textarea>
       </div>
       <label class="grid grid-cols-[minmax(120px,auto)_1fr] items-baseline">
         <strong>Hourly rate:</strong>
-        <input type="number" v-model.number="coach.hourlyRate" />
+        <input
+          type="number"
+          v-model.number="validateObj.hourlyRate.val"
+          :class="{
+            [invalidClass]: !validateObj.hourlyRate.isValid
+          }"
+          @change="removeValidate('hourlyRate')"
+        />
       </label>
       <base-button mode="contained" type="submit" class="self-end min-w-40">Submit </base-button>
     </form>
@@ -45,17 +83,63 @@ const router = useRouter()
 
 const store = useStore(key)
 const areas: TArea[] = ['backend', 'frontend', 'fullstack']
-const coach = ref<TCoach>({
-  id: '',
-  firstName: '',
-  lastName: '',
-  areas: [],
-  description: '',
-  hourlyRate: 0
+
+const invalidClass = 'border-red-500 focus:ring-red-500 focus:border-red-500'
+const validateObj = ref({
+  firstName: {
+    val: '',
+    isValid: true
+  },
+  lastName: {
+    val: '',
+    isValid: true
+  },
+  areas: {
+    val: [],
+    isValid: true
+  },
+  description: {
+    val: '',
+    isValid: true
+  },
+  hourlyRate: {
+    val: 0,
+    isValid: true
+  }
 })
 
+const removeValidate = (key: keyof typeof validateObj.value) => {
+  validateObj.value[key].isValid = true
+}
+
+const doValidate = () => {
+  validateObj.value.firstName.isValid = validateObj.value.firstName.val !== ''
+  validateObj.value.lastName.isValid = validateObj.value.lastName.val !== ''
+  validateObj.value.areas.isValid = validateObj.value.areas.val.length !== 0
+  validateObj.value.description.isValid = validateObj.value.description.val !== ''
+  validateObj.value.hourlyRate.isValid = validateObj.value.hourlyRate.val > 0
+  return (
+    validateObj.value.firstName.isValid &&
+    validateObj.value.lastName.isValid &&
+    validateObj.value.areas.isValid &&
+    validateObj.value.description.isValid &&
+    validateObj.value.hourlyRate.isValid
+  )
+}
+
 const onSubmit = () => {
-  store.dispatch('COACHES/addAction', coach.value)
+  if (!doValidate()) {
+    return
+  }
+  const data: TCoach = {
+    id: new Date().getTime().toString(),
+    firstName: validateObj.value.firstName.val,
+    lastName: validateObj.value.lastName.val,
+    areas: validateObj.value.areas.val,
+    description: validateObj.value.description.val,
+    hourlyRate: validateObj.value.hourlyRate.val
+  }
+  store.dispatch('COACHES/addAction', data)
   window.addEventListener('ADD_COACH_SUCCESS', () => {
     router.push({
       path: '/coaches'
@@ -63,5 +147,3 @@ const onSubmit = () => {
   })
 }
 </script>
-
-<style scoped></style>
