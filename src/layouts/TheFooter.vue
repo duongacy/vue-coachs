488db202f3f84f4a12698b922bd032499411246f
Lê Ý <duongy96@gmail.com>
2024-03-08 00:41:57 +0700

    Update layout

--- src/layouts/TheFooter.vue
+++ src/layouts/TheFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="bg-white py-4">
     <p class="mt-10 text-center text-xs leading-5 text-neutral-500">
-      &copy; 2024 YLVN, Inc. All rights reserved.
+      &copy; 2024 WTVue, Inc. All rights reserved.
     </p>
   </footer>
 </template>
--- src/layouts/TheNavigation.vue
+++ src/layouts/TheNavigation.vue
@@ -1,17 +1,6 @@
 <template>
-  <!-- <div class="flex h-12 justify-between items-center shadow-md">
-    <div>Find a Coach</div>
-    <div class="flex space-x-4">
-      <nav-link to="/common">Common</nav-link>
-      <nav-link to="/examples">Examples</nav-link>
-      <nav-link to="/coaches">Coaches</nav-link>
-      <nav-link to="/requests-received" v-if="isAuthenticated">Requests received</nav-link>
-      <nav-link to="/auth" v-if="!isAuthenticated">Login</nav-link>
-      <button @click="logout" v-else>Logout</button>
-    </div>
-  </div> -->
   <div class="flex h-12 items-center gap-8">
-    <BaseTypography variant="p" weight="bold">YLVN-vue</BaseTypography>
+    <BaseTypography variant="p" weight="bold">WTVue</BaseTypography>
     <div class="flex flex-grow gap-4 px-4">
       <router-link to="/coaches">Coaches</router-link>
       <router-link to="/requests-received">Requests received</router-link>
@@ -30,10 +19,9 @@ import { BaseTypography } from '@/components/ui/typography'
 import { store } from '@/store'
 import { computed } from 'vue'
 import { useRouter } from 'vue-router'
+
 const router = useRouter()
-
 const isAuthenticated = computed(() => store.getters['AUTHEN/isAuthenticated'])
-
 const logout = async () => {
   try {
     await store.dispatch('AUTHEN/signout')
@@ -41,7 +29,6 @@ const logout = async () => {
     alert('Logout error')
   }
 }
-
 const loginHandler = () => {
   router.push('/auth')
 }
