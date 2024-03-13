c57084d9a90729d3bbbb99bfb0c13064ff62bf5c
Lê Ý <duongy96@gmail.com>
2024-03-13 19:27:45 +0700

    Update login page

--- src/pages/auth/SignInForm.vue
+++ src/pages/auth/SignInForm.vue
@@ -1,32 +1,31 @@
 <script setup lang="ts">
-import { BaseButton } from '@/components/ui/button';
-import { BaseInput } from '@/components/ui/input';
-import { BaseTypography } from '@/components/ui/typography';
-import { EMAIL_REGEX, PASSWORD_REGEX } from '@/constants';
-import { key } from '@/store';
-import type { authActions } from '@/store/modules/AUTHEN/actions';
-import { toTypedSchema } from '@vee-validate/zod';
-import { useForm } from 'vee-validate';
-import { onBeforeRouteLeave } from 'vue-router';
-import { useStore } from 'vuex';
-import * as z from 'zod';
-
+import { BaseButton } from '@/components/ui/button'
+import { BaseInput } from '@/components/ui/input'
+import { BaseTypography } from '@/components/ui/typography'
+import { EMAIL_REGEX, PASSWORD_REGEX } from '@/constants'
+import { key } from '@/store'
+import type { authActions } from '@/store/modules/AUTHEN/actions'
+import { toTypedSchema } from '@vee-validate/zod'
+import { useForm } from 'vee-validate'
+import { onBeforeRouteLeave } from 'vue-router'
+import { useStore } from 'vuex'
+import * as z from 'zod'
 const store = useStore(key)
 
 const zodSchema = z.object({
-  email: z.string({ coerce: true }).refine(value => EMAIL_REGEX.test(value), 'Your email is invalid'),
-  password: z.string({ coerce: true }).refine(value => PASSWORD_REGEX.test(value), 'Your password is invalid'),
+  email: z.string().refine((value) => EMAIL_REGEX.test(value), 'Your email is invalid'),
+  password: z.string().refine((value) => PASSWORD_REGEX.test(value), 'Your password is invalid')
 })
 
 type LoginForm = z.infer<typeof zodSchema>
 
-const { errors, handleSubmit, defineField, values } = useForm<LoginForm>({
+const { errors, handleSubmit, defineField } = useForm<LoginForm>({
   initialValues: { email: '', password: '' },
-  validationSchema: toTypedSchema(zodSchema),
+  validationSchema: toTypedSchema(zodSchema)
 })
 
-const [email] = defineField('email');
-const [password] = defineField('password');
+const [email] = defineField('email')
+const [password] = defineField('password')
 
 const onSubmit = handleSubmit(
   async (data) => {
@@ -41,13 +40,15 @@ const onSubmit = handleSubmit(
     }
   },
   (e) => {
-    const firstErrorField = document.querySelector(`[name='${Object.keys(e.errors)[0]}']`) as HTMLInputElement
+    const firstErrorField = document.querySelector(
+      `[name='${Object.keys(e.errors)[0]}']`
+    ) as HTMLInputElement
     firstErrorField?.focus()
   }
 )
 
 onBeforeRouteLeave(() => {
-  return window.confirm("Do you want to leave?");
+  return window.confirm('Do you want to leave?')
 })
 </script>
 <template>
@@ -62,7 +63,7 @@ onBeforeRouteLeave(() => {
     <form class="mt-6 space-y-4" @submit.prevent="onSubmit">
       <div class="space-y-3">
         <div>
-          <BaseInput placeholder="Enter your email" v-model="email" name="email" />
+          <BaseInput placeholder="Enter your email" v-model:model-value="email" name="email" />
           <BaseTypography class="mt-1 text-red-500" variant="small" v-if="!!errors.email">
             {{ errors.email }}
           </BaseTypography>
