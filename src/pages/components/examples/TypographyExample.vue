37c85fce913606d510455967c96e9f77e4fb9d49
Lê Ý <duongy96@gmail.com>
2024-03-16 01:37:21 +0700

    Update typo

--- src/pages/components/examples/TypographyExample.vue
+++ src/pages/components/examples/TypographyExample.vue
@@ -1,5 +1,44 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const fontWeightClasses = [
+  'font-black',
+  'font-extrabold',
+  'font-bold',
+  'font-semibold',
+  'font-medium',
+  'font-normal',
+  'font-light',
+  'font-extralight',
+  'font-thin',
+];
+const textClasses = [
+  'text-heading1',
+  'text-heading2',
+  'text-heading3',
+  'text-heading4',
+  'text-heading5',
+  'text-heading6',
+  'text-body',
+  'text-caption',
+  'text-small',
+  'text-tiny',
+];
+</script>
 
 <template>
-  <div class="flex flex-wrap items-start gap-4"></div>
+  <div class="grid">
+    <div
+      v-for="fontWeightClass in fontWeightClasses"
+      :key="fontWeightClass"
+      class="flex items-baseline gap-4"
+    >
+      <p class="w-20">{{ fontWeightClass.split('-')[1] }}</p>
+      <p
+        v-for="textClass in textClasses"
+        :key="textClass"
+        :class="[textClass, fontWeightClass]"
+      >
+        {{ textClass.split('-')[1] }}
+      </p>
+    </div>
+  </div>
 </template>
