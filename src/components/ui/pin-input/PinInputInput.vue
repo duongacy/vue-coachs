efa9cf8c7f63d8f56932745337b6d5021c2f1199
Lê Ý <duongy96@gmail.com>
2024-03-17 16:08:18 +0700

    Pin input

--- src/components/ui/pin-input/PinInputInput.vue
+++ src/components/ui/pin-input/PinInputInput.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { cn } from '@/lib/utils';
 import { PinInputInput, useForwardProps, type PinInputInputProps } from 'radix-vue';
-import { computed, type HTMLAttributes } from 'vue';
+import { computed, toRef, type HTMLAttributes } from 'vue';
 
-const props = defineProps<PinInputInputProps & { class?: HTMLAttributes['class'] }>();
+const props = defineProps<
+  PinInputInputProps & { class?: HTMLAttributes['class']; isError?: boolean }
+>();
 
 const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props;
+  const { class: _, isError, ...delegated } = props;
   return delegated;
 });
 
@@ -19,6 +21,7 @@ const forwardedProps = useForwardProps(delegatedProps);
     :class="
       cn(
         'flex h-10 w-10 rounded-md border border-input bg-background text-center text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+        { 'border-destructive text-destructive focus-visible:ring-destructive': isError },
         props.class,
       )
     "
--- src/pages/components/examples/PINInputExample.vue
+++ src/pages/components/examples/PINInputExample.vue
@@ -1,25 +1,66 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import BaseLabel from '@/components/ui/label/BaseLabel.vue';
 import { PinInput, PinInputInput } from '@/components/ui/pin-input';
+import { ref } from 'vue';
 
 const value = ref<string[]>([]);
-const handleComplete = (e: string[]) => alert(e.join(''));
+const handleComplete = (e: string[]) => {
+  setTimeout(() => {
+    alert(e.join(''));
+  }, 1);
+};
+
+const error = ref(true);
+const onReset = () => {
+  value.value = ['', '', '', '', '', ''];
+  error.value = false;
+};
 </script>
 
 <template>
   <div class="grid gap-2">
-    <PinInput
-      id="pin-input"
-      v-model="value"
-      placeholder="○"
-      class="mt-1 flex items-center gap-2"
-      @complete="handleComplete"
-    >
-      <PinInputInput
-        v-for="(id, index) in 5"
-        :key="id"
-        :index="index"
-      />
-    </PinInput>
+    <div class="grid gap-2">
+      <p class="text-heading6 font-bold">Default</p>
+      <BaseLabel class="grid gap-1">
+        <span>OTP</span>
+        <PinInput
+          id="pin-input"
+          v-model="value"
+          placeholder="○"
+          class="mt-1 flex items-center gap-2"
+          @complete="handleComplete"
+          type="number"
+        >
+          <PinInputInput
+            v-for="(id, index) in 5"
+            :key="id"
+            :index="index"
+          />
+        </PinInput>
+      </BaseLabel>
+    </div>
+    <div class="grid gap-2">
+      <p class="text-heading6 font-bold">Error</p>
+      <BaseLabel
+        class="grid gap-1"
+        @click="onReset"
+      >
+        <span>OTP</span>
+        <PinInput
+          id="pin-input"
+          v-model="value"
+          placeholder="○"
+          class="mt-1 flex items-center gap-2"
+          type="number"
+        >
+          <PinInputInput
+            v-for="(id, index) in 5"
+            :key="id"
+            :index="index"
+            :isError="error"
+          />
+        </PinInput>
+      </BaseLabel>
+    </div>
   </div>
 </template>
